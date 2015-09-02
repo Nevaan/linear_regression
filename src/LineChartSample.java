72a4a551d605606703ccf8811f135975525170ce
Kabece <kabece@gmail.com>
2015-09-02 06:54:45 +0200

    no message

--- src/LineChartSample.java
+++ src/LineChartSample.java
@@ -11,23 +11,25 @@ import regression.Population;
 import targetRepresentation.Cartesian;
 import targetRepresentation.GPParameters;
 import treeRepresentation.TreeNode;
- 
- 
+
+
 public class LineChartSample extends Application {
 
     @Override public void start(Stage stage) throws Exception{
         stage.setTitle("Line Chart Sample");
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Month");       
-        
-        final ScatterChart<Number,Number> lineChart = 
+        xAxis.setLabel("Month");
+
+        final ScatterChart<Number,Number> lineChart =
                 new ScatterChart<Number,Number>(xAxis,yAxis);
-                
+
+        Cartesian.init();
+
 		Population population = new Population();
 		population.initializePopulation();
 		TreeNode fittest = population.getFittest().getSchema();
-        
+
 		for(int i = 0; i < GPParameters.GENERATIONS_AMOUNT; i++) {
 			population = Genetics.evolve(population);
 			System.out.println("~~~~~~~~~~~" + i + " Population ~~~~~~~~~~~~");
@@ -35,21 +37,21 @@ public class LineChartSample extends Application {
 			//TreeGraphView.displayTreeGraph(population.getFittest().getSchema(), "Fittest");
 			System.out.println(population.getFittest().getSchema().printFunction());
 		}
-		
+
         lineChart.setTitle("Guwno gandora");
 
         XYChart.Series series = new XYChart.Series();
         series.setName("Wykres");
 
-        
+
         Cartesian uno = new Cartesian();
-        for (int i = 0; i< uno.getBoard().size();i++) {
-        	
-        	series.getData().add(new XYChart.Data(uno.getBoard().get(i).getX(), uno.getBoard().get(i).getY()));
+        for (int i = 0; i< GPParameters.board.size();i++) {
+
+        	series.getData().add(new XYChart.Data(GPParameters.board.get(i).getX(), GPParameters.board.get(i).getY()));
         }
 
 
-        
+
         Scene scene  = new Scene(lineChart,800,600);
         lineChart.getData().add(series);
 
