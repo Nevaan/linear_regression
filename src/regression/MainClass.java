d47c321f78e5a9cd74836bb0e7ceac5327b5db26
unknown <kczurylo@kczurylo-lap.krakow.qumak.pl>
2015-09-07 15:58:38 +0200

    Prototype of xml querying.

--- src/regression/MainClass.java
+++ src/regression/MainClass.java
@@ -2,16 +2,19 @@ package regression;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import genetics.Genetics;
-import genetics.Population;
-import graphics.graphs.TreeGraphView;
-import treeRepresentation.ClassToXML;
-import treeRepresentation.TreeGenerator;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import treeElement.function.Add;
+import treeElement.function.Divide;
+import treeElement.function.Multiply;
+import treeElement.function.Substract;
+import treeElement.terminal.Constant;
+import treeElement.terminal.Variable;
+import treeRepresentation.QueryXML;
 import treeRepresentation.TreeNode;
-import treeRepresentation.XMLtoClass;
 
 public class MainClass {
 
@@ -19,31 +22,78 @@ public class MainClass {
 
 		// Odkomentowaæ przy pierwszym odpaleniu - wrzuca do katalogu xml pierwsza populacje; potem mozna zakomentowac i zostawic to co jest nizej zeby zobaczyc,
 		// ze poprawnie wczytuje sie ta populacja
-		
-		//directoryCleanUp();		
+
+		//directoryCleanUp();
 		//Population population = new Population();
 		//population.initialize(0);
-		
-		
-		// Odkomentowac po min. jednokrotnym odpaleniu powyzszego - inaczej wywala error bo brakuje xmli 
-		/*
-		for (int i = 0; i<Parameters.POPULATION_SIZE; i++){
-			TreeNode loadedChromosome = XMLtoClass.convert(i,0);	
+
+
+		// Odkomentowac po min. jednokrotnym odpaleniu powyzszego - inaczej wywala error bo brakuje xmli
+
+		/*for (int i = 0; i<Parameters.POPULATION_SIZE; i++){
+			TreeNode loadedChromosome = XMLtoClass.convert(i,0);
 			TreeGraphView.displayTreeGraph(loadedChromosome , "Test ");
-		}*/
-		
-		
+		}
+
+
 		Genetics g = new Genetics();
-		g.evolve(1);
+		g.evolve(1);*/
+
+
+
+		//**************************************************
+
+
+
+		/*TreeNode testTree = null;
+		TreeNode testTree2 = null;
+
+		try {
+			testTree = TreeGenerator.generateGrowTree(Parameters.GROW_TREE_MAX_DEPTH);
+			testTree2 = TreeGenerator.generateGrowTree(Parameters.GROW_TREE_MAX_DEPTH);
+		} catch (Exception e) {
+			System.out.println("Error while generating test tree.");
+			e.printStackTrace();
+		}
+
+		//TreeGraphView.displayTreeGraph(testTree, "Test tree");
+		//TreeGraphView.displayTreeGraph(testTree2, "Test tree");
+
+		ClassToXML.convert(testTree, 0);
+		ClassToXML.convert(testTree2, 0);*/
+
+
+		//**************************************************
+
+		QueryXML process = new QueryXML();
+		try {
+			process.query();
+
+
+
+
+			File currentDir = new File(".");
+			File file = new File(currentDir + "/xml/lol.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Add.class, Divide.class, Multiply.class, Substract.class, Constant.class, Variable.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			TreeNode treeNode = (TreeNode) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(treeNode);
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	
+
 	public static void directoryCleanUp(){
 		if(!Files.exists(Paths.get("./xml/"))){
 			new File("./xml/").mkdir();
 		}
-		
+
 		File folder = new File("./xml/");
 		File[] files = folder.listFiles();
 		if(files!=null){
